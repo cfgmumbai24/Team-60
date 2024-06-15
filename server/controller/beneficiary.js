5626513e4af7fbc532baad552f19519031b2960c
MSP20086 <mihirpandit04@gmail.com>
2024-06-15 23:32:25 +0530

    final

--- server/controller/beneficiary.js
+++ server/controller/beneficiary.js
@@ -1,43 +1,52 @@
 import Goat from "../models/goats.js";
+import Beneficiary from "../models/beneficiary.js";
 
 const getGoats = async (req, res) => {
     const { _id } = req.params;
-    const Beneficiary = await Beneficiary.findById(_id);
+    const beneficiary = await Beneficiary.findById(_id);
 
-    if (!Beneficiary) {
+    if (!beneficiary) {
         res.status(404).json({ message: "Beneficiary not found" });
     }
     else {
-        res.status(200).json(Beneficiary.goats);
+        res.status(200).json(beneficiary.goats);
     }
 }
 
 const addGoat = async (req, res) => {
     const { _id } = req.params;
-    const { tag, age, vaccinated, weight } = req.body;
-    const Beneficiary = await Beneficiary.findById(_id);
+    const { tag, age, vaccinated, weight, gender } = req.body;
+    const beneficiary = await Beneficiary.findById(_id);
 
-    if (!Beneficiary) {
+    if (!beneficiary) {
         res.status(404).json({ message: "Beneficiary not found" });
     }
     else {
-        const newGoat = new Goat({ tag, age, vaccinated, weight });
+        const newGoat = new Goat({ tag, age, vaccinated, weight, gender });
         await newGoat.save();
-        Beneficiary.goats.push(newGoat._id);
-        await Beneficiary.save();
+        beneficiary.goats.push(newGoat._id);
+        await beneficiary.save();
         res.status(201).json(newGoat);
     }
 }
 
 const updateGoatHealth = async (req, res) => {
     const { tag, age, vaccinated, weight } = req.body;
-    const goat = Goats.findOne({ tag: tag });
-    if (!goat) {
-        res.status(404).json({ message: "Goat not found" });
+
+    try {
+        const goat = await Goat.findOne({ tag: tag });
+
+        if (!goat) {
+            return res.status(404).json({ message: "Goat not found" });
+        }
+
+        const updatedGoat = await Goat.findByIdAndUpdate(goat._id, { age, vaccinated, weight }, { new: true });
+
+        return res.status(200).json(updatedGoat);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "An error occurred while updating the goat's health." });
     }
-    else {
-        const updatedGoat = Goats.findByIdAndUpdate(goat._id, { age, vaccinated, weight }, { new: true });
-    }
-}
+};
 
 export default { getGoats, addGoat, updateGoatHealth }
--- server/routes/beneficiary.js
+++ server/routes/beneficiary.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import beneficiary from '../controller/beneficiary.js';
 const router = Router();
 
-router.get('/getGoats', beneficiary.getGoats);
-router.post('/addGoat', beneficiary.addGoat);
+router.get('/getGoats/:_id', beneficiary.getGoats);
+router.post('/addGoat/:_id', beneficiary.addGoat);
 router.put('/updateGoatHealth', beneficiary.updateGoatHealth);
 
 export default router;
